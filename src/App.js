69e004e84dcbab4351ec36b93affdcb47cfeb464
Dandea <alessandromanueldandea@gmail.com>
2025-01-24 15:16:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Ciao, React e Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
