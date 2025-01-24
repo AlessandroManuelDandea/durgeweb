c7c43b43d69b9e2661df89fafab1158db1841446
Dandea <alessandromanueldandea@gmail.com>
2025-01-24 15:45:45 +0100

    Remove build from .gitignore and add build to the repository

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './index.css';
 function App() {
   return (
     <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Ciao, React e Tailwind!</h1>
+      <h1 className="text-4xl font-blackletter">Ciao, questo è il font Blackletter!</h1>
+      <p className="font-horstblackletter">Questo è il font Horst Blackletter.</p>
+      <p className="font-blaceb">E questo è il font BLACEB.</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        blackletter: ['Blackletter', 'serif'],
+        horstblackletter: ['HorstBlackletter', 'serif'],
+        blaceb: ['BLACEB', 'serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
